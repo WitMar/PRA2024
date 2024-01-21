d004aecfa76ffb37959080df7c2eed6f64a8094a
marcin witkowski <marcin.witkowski@e-qsg.com>
2024-01-21 23:50:37 +0100

    Classes functional components React start

--- src/App.js
+++ src/App.js
@@ -2,28 +2,15 @@ import './App.css';
 import React from "react";
 import StateExample from "./StateExample/StateExample";
 import {ThemeContext} from "./ContextExample/ThemeProvider";
-import Form from "./Form/Form";
 
 function App() {
 
     const {theme, toggle, dark} = React.useContext(ThemeContext)
 
     return (
-        <div className="App" style={{backgroundColor: theme.backgroundColor, color: theme.color}}>
+        <div>
             Test
             <StateExample/>
-            <button
-                type="button"
-                onClick={toggle}
-                style={{
-                    backgroundColor: theme.backgroundColor,
-                    color: theme.color,
-                    outline: 'none'
-                }}
-            >
-                Toggle to {!dark ? 'Dark' : 'Light'} theme
-            </button>
-            <Form/>
         </div>
     );
 }
--- src/StateExample/StateExample.js
+++ src/StateExample/StateExample.js
@@ -1,29 +1,16 @@
-import React, {useEffect, useState} from 'react';
-import useCustomExample from "../CustomExample/useCustomExample";
+import React, {useState} from 'react';
 
 function StateExample() {
     const [count, setCount] = useState(0);
-    const [refreshed, setRefreshed] = useState(0);
-    const word = useCustomExample(count);
 
-    useEffect(() => {    // Update the document title using the browser API    d
-        setRefreshed(refreshed + 1);
-        return () => {
-            console.log("DESTROYED 1");
-        }
-    }, [count]);
-
-
-    return (
+     return (
         <div>
             <p>You clicked {count} times</p>
             <button onClick={() => setCount(count + 1)}>
                 Click me
             </button>
             <p></p>
-            Odświeżono komponent state {refreshed} razy
             <p></p>
-            {word}
         </div>
     );
 }
